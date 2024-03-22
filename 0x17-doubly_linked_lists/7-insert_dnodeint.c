8a8b556f1997c8b0fea01e5c5c2fbd033d038791
Sussanne <sussannetiana@gmail.com>
2024-03-21 19:07:22 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
@@ -11,55 +11,46 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    if (h == NULL)  /* Check if the pointer to the head is NULL */
+    dlistint_t *new_node, *temp;
+    unsigned int i;
+
+    if (h == NULL)
         return NULL;
 
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));  /* Create the new node */
+    new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
         return NULL;
-
+    
     new_node->n = n;
     new_node->prev = NULL;
     new_node->next = NULL;
 
-    if (*h == NULL && idx != 0)  /* List is empty, and index is not 0 */
-    {
-        free(new_node);
-        return NULL;
-    }
-
-    if (idx == 0)  /* Add at the beginning */
+    if (idx == 0)
     {
         if (*h != NULL)
+        {
+            new_node->next = *h;
             (*h)->prev = new_node;
-        new_node->next = *h;
+        }
         *h = new_node;
         return new_node;
     }
 
-    dlistint_t *current = *h;
-    unsigned int i = 0;
+    temp = *h;
+    for (i = 0; temp != NULL && i < idx - 1; i++)
+        temp = temp->next;
 
-    while (current != NULL && i < idx - 1)  /* Traverse to the node at index idx - 1 */
-    {
-        current = current->next;
-        i++;
-    }
-
-    if (current == NULL)  /* Index is out of bounds */
+    if (temp == NULL)
     {
         free(new_node);
         return NULL;
     }
 
-    /* Adjust pointers */
-    new_node->prev = current;
-    new_node->next = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = new_node;
-
-    current->next = new_node;
+    new_node->next = temp->next;
+    if (temp->next != NULL)
+        temp->next->prev = new_node;
+    temp->next = new_node;
+    new_node->prev = temp;
 
     return new_node;
 }
