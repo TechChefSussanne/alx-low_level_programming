3f5128c37595728193a1dc1a4052ff9d3155c3a8
Sussanne <sussannetiana@gmail.com>
2024-03-21 19:01:41 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -12,24 +11,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    if (h == NULL)
+    if (h == NULL)  /* Check if the pointer to the head is NULL */
         return NULL;
 
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));  /* Create the new node */
     if (new_node == NULL)
         return NULL;
-    
+
     new_node->n = n;
     new_node->prev = NULL;
     new_node->next = NULL;
 
-    if (idx == 0)
+    if (*h == NULL && idx != 0)  /* List is empty, and index is not 0 */
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    if (idx == 0)  /* Add at the beginning */
     {
         if (*h != NULL)
-        {
-            new_node->next = *h;
             (*h)->prev = new_node;
-        }
+        new_node->next = *h;
         *h = new_node;
         return new_node;
     }
@@ -37,76 +40,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *current = *h;
     unsigned int i = 0;
 
-    while (current != NULL && i < idx - 1)
+    while (current != NULL && i < idx - 1)  /* Traverse to the node at index idx - 1 */
     {
         current = current->next;
         i++;
     }
 
-    if (current == NULL)
+    if (current == NULL)  /* Index is out of bounds */
     {
         free(new_node);
         return NULL;
     }
 
+    /* Adjust pointers */
+    new_node->prev = current;
     new_node->next = current->next;
+
     if (current->next != NULL)
         current->next->prev = new_node;
 
     current->next = new_node;
-    new_node->prev = current;
 
     return new_node;
-}
-
-/* Function to print a doubly linked list */
-void print_dlistint(dlistint_t *h)
-{
-    while (h != NULL)
-    {
-        printf("%d ", h->n);
-        h = h->next;
-    }
-    printf("\n");
-}
-
-/* Function to free memory allocated for a doubly linked list */
-void free_dlistint(dlistint_t *head)
-{
-    dlistint_t *temp;
-
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-}
-
-int main(void)
-{
-    dlistint_t *head = NULL;
-
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
-
-    printf("Original list: ");
-    print_dlistint(head);
-
-    printf("-----------------\n");
-
-    insert_dnodeint_at_index(&head, 5, 4096);
-
-    printf("After inserting at index 5: ");
-    print_dlistint(head);
-
-    free_dlistint(head);
-
-    return (EXIT_SUCCESS);
 }
