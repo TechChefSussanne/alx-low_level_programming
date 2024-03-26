19e543b80a93c5f05790d4e942db899ad1f60629
Sussanne <sussannetiana@gmail.com>
2024-03-25 18:41:32 -0700

    libdynamic.so

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,7 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,7 +23,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
+#endif /* MAIN_H */
 
-#endif /* __MAIN__ */
